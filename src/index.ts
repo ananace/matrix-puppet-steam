d049eac651508138337e94297f909c4e285e6dd4
Alexander "Ace" Olofsson <ace@haxalot.com>
2017-10-10 07:41:55 +0200

    Start off with a reasonable indentation

--- src/index.ts
+++ src/index.ts
@@ -11,56 +11,56 @@ import { SteamUser } from 'steam-user';
 import { UINT64 as UInt64 } from 'cuint';
 
 export class Adapter extends ThirdPartyAdapter {
-    public serviceName = 'Steam';
-    private client : SteamUser = null;
+  public serviceName = 'Steam';
+  private client : SteamUser = null;
 
-    startClient(): Promise<void> {
-        this.client = new SteamUser({
-            promptSteamGuardCode: false // Should be possible to route to the client
-        });
+  startClient(): Promise<void> {
+    this.client = new SteamUser({
+      promptSteamGuardCode: false // Should be possible to route to the client
+    });
 
-        return new Promise((resolve, reject) => {
-            this.client.once('loggedOn', resolve);
-            this.client.once('steamGuard', reject);
-            this.client.once('error', reject);
-            
-            this.client.logOn({
-                accountName: 'account',
-                loginKey: 'accountToken',
-                logonID: 12345, // Perhaps base on the mxid somehow.
-                machineName: 'Matrix Puppet-Bridge',
-            });
-        });
-    }
+    return new Promise((resolve, reject) => {
+      this.client.once('loggedOn', resolve);
+      this.client.once('steamGuard', reject);
+      this.client.once('error', reject);
+      
+      this.client.logOn({
+        accountName: 'account',
+        loginKey: 'accountToken',
+        logonID: 12345, // Perhaps base on the mxid somehow.
+        machineName: 'Matrix Puppet-Bridge',
+      });
+    });
+  }
 
-    sendMessage(thirdPartyRoomId: string, text: string): Promise<void> {
-        return new Promise((resolve, reject) => {
-            let roomId = new UInt64(thirdPartyRoomId, 36).toString();
-            if (this.client.chatMessage(roomId, text))
-                resolve();
-            else
-                reject();
-        });
-    }
+  sendMessage(thirdPartyRoomId: string, text: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+      let roomId = new UInt64(thirdPartyRoomId, 36).toString();
+      if (this.client.chatMessage(roomId, text))
+        resolve();
+      else
+        reject();
+    });
+  }
 
-    sendImageMessage(thirdPartyRoomId: string, Image): Promise<void> {
-        return Promise.reject(null);
-    }
+  sendImageMessage(thirdPartyRoomId: string, Image): Promise<void> {
+    return Promise.reject(null);
+  }
 
-    getRoomData(thirdPartyRoomId: string): Promise<RoomData> {
-        return new Promise<RoomData>((resolve, reject) => {
-            let payload = <RoomData>{
-                name: '',
-                topic: '',
-                avatarUrl: '',
-                isDirect: false,
-            };
+  getRoomData(thirdPartyRoomId: string): Promise<RoomData> {
+    return new Promise<RoomData>((resolve, reject) => {
+      let payload = <RoomData>{
+        name: '',
+        topic: '',
+        avatarUrl: '',
+        isDirect: false,
+      };
 
-            resolve(payload);
-        });
-    }
+      resolve(payload);
+    });
+  }
 
-    getUserData(thirdPartyUserId: string): Promise<UserData> {
-        return null;
-    }
+  getUserData(thirdPartyUserId: string): Promise<UserData> {
+    return null;
+  }
 }
