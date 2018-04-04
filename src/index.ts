2c21a02901c8022c1ea15d2353877d896880cad5
Alexander Olofsson <ace@haxalot.com>
2018-04-04 22:47:59 +0200

    adapter: Cleaning up and adding some more code

--- src/index.ts
+++ src/index.ts
@@ -27,7 +27,7 @@ export class Adapter extends ThirdPartyAdapter {
         return <ContactListUserData>{
           name: f.player_name,
           userId: f.friendid.toString(36),
-          avatarUrl: f.avatar_url_icon // XXX: Maybe use a larger avatar
+          avatarUrl: f.avatar_url_full
         };
       }));
     });
@@ -61,14 +61,23 @@ export class Adapter extends ThirdPartyAdapter {
 
   startClient(): Promise<void> {
     return new Promise((resolve, reject) => {
-      this.client.once('loggedOn', resolve);
-      this.client.once('steamGuard', reject); // XXX: Do !steamguard
-      this.client.once('error', reject);
+      this.client.on('loggedOn', (err) => {
+        this.puppetBridge.sendStatusMsg({}, 'connected');
+
+        this.client.setPersona(SteamUser.EPersonaState.Online);
+      });
+      this.client.once('steamGuard', (err) => {
+        this.puppetBridge.sendStatusMsg({}, `TODO: Steam guard is enabled, can't connect.`);
+      });
+      this.client.once('error', (err) => {
+        this.puppetBridge.sendStatusMsg({}, `unable to connect to steam; ${err}`);
+      });
       
-      this.client.logOn({
-        accountName: 'account',
-        loginKey: 'accountToken',
-        logonID: 12345, // Perhaps base on the mxid somehow?
+      return this.client.logOn({
+        accountName: this.config.account_name,
+        password: this.config.password,
+        loginKey: this.config.login_key,
+        logonID: this.config.login_id || 12345, // Perhaps base on the mxid somehow?
         machineName: 'Matrix Puppet-Bridge',
       });
     });
@@ -98,12 +107,39 @@ export class Adapter extends ThirdPartyAdapter {
         let payload = <RoomData>{
           name: friend.player_name,
           topic: 'Steam Friend',
-          avatarUrl: friend.avatar_url_icon,
+          avatarUrl: friend.avatar_url_full,
           isDirect: true,
         };
 
         resolve(payload);
       } else {
+        if (sid.getSteamID64() in this.client.chats)
+        {
+          let chat = this.client.chats[sid.getSteamID64()];
+
+          let payload = <RoomData>{
+            name: room.name,
+            topic: 'Steam Chat',
+            isDirect: false,
+          };
+
+          if (sid.isGroupChat()) {
+            let groupId = new SteamID(sid);
+            groupId.type = SteamID.Type.CLAN;
+            groupId.instance &= ~SteamID.ChatInstanceFlags.Clan;
+
+            let group = this.client.groups[groupId];
+            if (group) {
+              let hash = group.name_info.sha_avatar;
+              if (hash === '0000000000000000000000000000000000000000') {
+                hash = 'fef49e7fa7e1997310d705b2a6158ff8dc1cdfeb';
+              }
+              payload.avatarUrl = `https://steamcdn-a.akamaihd.net/steamcommunity/public/images/avatars/${hash.substring(0,2)}/${hash}_full.jpg`;
+            } else {
+              // TODO: Get group information somehow
+            }
+          }
+        }
       }
     });
   }
@@ -118,12 +154,21 @@ export class Adapter extends ThirdPartyAdapter {
 
       if (sid.getSteamID64() in this.client.users) {
         let friend = this.client.users[sid.getSteamID64()];
-        let payload = <UserData>{
-          name: friend.player_name,
-          avatarUrl: friend.avatar_url_full,
-        };
 
-        resolve(payload);
+        if (friend) {
+          resolve(<UserData> {
+            name: friend.player_name,
+            avatarUrl: friend.avatar_url_full,
+          });
+        } else {
+          this.client.getPersonas([sid], (result) => {
+            friend = result[sid];
+            resolve(<UserData> {
+              name: friend.player_name,
+              avatarUrl: friend.avatar_url_full,
+            });
+          });
+        }
       }
     });
   }
